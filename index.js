8a6a06e68b9bc9b47c1c85a6128c105181ada9f2
Luis Gabriel Araújo <harrycorpmail@gmail.com>
2022-09-15 11:30:11 -0300

    🆕 - Creating main file

---
+++ index.js
@@ -0,0 +1,188 @@
+/*
+MIT License
+Copyright (c) 2022 Luis Gabriel Araújo
+
+*/
+
+const express = require("express"); // App
+const saldoRU = require("saldo-ru-ufc");
+
+// App Configs
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => res.send("<h1>Hello World!</h1>"));
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
+
+// Telegraf Modules
+const { Markup, Scenes, session, Telegraf } = require("telegraf");
+const token = process.env['token'];
+const bot = new Telegraf(token);
+const scene = new Scenes.BaseScene("example");
+const stage = new Scenes.Stage([scene]);
+
+// Conectar com o banco
+const mongoose = require("mongoose");
+const userSchema = require("./schemas/user");
+const https = require("https");
+const mongoServer = process.env['mongoServer'];
+
+
+mongoose.connect(
+  mongoServer,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+);
+
+bot.use(session());
+bot.use(stage.middleware());
+
+
+let stopListen;
+let messageId;
+bot.command("/vincular", async (ctx) => {
+  stopListen = false;
+
+      ctx.replyWithMarkdown(`Certo, agora vincularemos o cartão e matrícula à sua conta do *Telegram*. Para isso, siga os passos abaixo:
+
+*1.* Escreva o número do seu cartão do *RU*;
+*2.* Dê espaço;
+*3.* Escreva o *número da sua matrícula*;
+*4.* Envie a mensagem.
+
+*Exemplo:*
+0123456789 123456
+`);
+
+  let userInfo = [];
+  
+scene.enter(async (ctx) => {
+
+  messageText = ctx.message.text;
+  arr = messageText.split(" ");
+  userId = ctx.message.from.id;
+  
+
+  saldoRU.saldo(arr[0], arr[1]).then(async (res) => {
+
+    userInfo = [userId, parseInt(arr[0]), parseInt(arr[1])];
+    if (res.nome) {
+          
+          return ctx.replyWithMarkdown(`Você confirma o vínculo ao cartão no nome de *${res.nome}*?`,
+            Markup.inlineKeyboard([
+              Markup.button.callback("Sim", "sim"),
+              Markup.button.callback("Não", "nao"),
+            ])).then(({ message_id }) => {
+              messageId = message_id;
+          });
+    } else {
+      await ctx.replyWithMarkdown(`*❌ ${res}*`);
+    }
+  
+      });
+  })
+  
+    bot.on('text', async (ctx) => {
+      
+      
+      if (stopListen == false) {
+      await ctx.scene.enter("example");
+
+        scene.action("sim", async (ctx) => {
+          // If Video In Cache, Send Quickly
+
+          await ctx.answerCbQuery("Vinculando...");
+
+          let usersData = await userSchema.find({
+            UserId: userInfo[0],
+          });       
+
+          if (usersData != '') {
+             let updateData = await userSchema.findOneAndUpdate(
+            {
+              UserId: userInfo[0]
+            },
+            {
+              CardNumber: userInfo[1],
+              Matricula: userInfo[2]
+            }, {
+              returnOriginal: false
+            });
+          } else {
+          let newData = new userSchema({
+            UserId: userInfo[0],
+            CardNumber: userInfo[1],
+            Matricula: userInfo[2]
+          })
+          newData.save();
+        }
+
+            await ctx.deleteMessage(messageId);
+            await ctx.replyWithMarkdown("*✅ Cartão vinculado com sucesso!* Digite */saldo* para consultá-lo.");
+          });
+          
+
+        scene.action("nao", async (ctx) => {
+        
+          await ctx.answerCbQuery("Vínculo cancelado!");
+          await ctx.deleteMessage(messageId);
+          await ctx.replyWithMarkdown(`*❌ Vínculo cancelado pelo usuário.*`);
+        });
+      }
+      stopListen = true;
+})
+  
+    });
+
+bot.command("/saldo", async (ctx) => {
+  messageUserId = ctx.message.from.id;
+  
+  let usersData = await userSchema.find({
+            UserId: messageUserId,
+          });       
+      
+  if (usersData != '') {
+    saldoRU.saldo(usersData[0].CardNumber, usersData[0].Matricula).then((res) => {
+      name = res.nome.toLowerCase();
+      finalName = name.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+      
+      ctx.replyWithMarkdown(`*💳 Informações do cartão*
+
+*Titular:* ${finalName}
+*Créditos:* ${res.creditos}`);
+    })
+  } else {
+    ctx.replyWithMarkdown(`❌ Parece que você ainda não vinculou nenhum cartão à sua conta. Para vincular, digite */vincular*.`)
+  }
+});
+
+bot.start((ctx) =>
+  ctx.replyWithMarkdown(
+    `🤖 Olá! Eu posso consultar o seu saldo do *cartão do RU* da *Universidade Federal do Ceará*, que utiliza o sistema *SIPAC*.
+Para iniciar a configuração, digite */vincular* e vincule o seu cartão.
+
+*Obs: O bot não possui nenhum vínculo com a Universidade Federal do Ceará.*`)
+);
+
+bot.help((ctx) => {
+  ctx.replyWithMarkdown(
+    `*📃 Os comandos disponíveis são:*
+
+*/vincular* - Vincular cartão e matrícula à sua conta do *Telegram*.
+*/saldo* - Consultar o saldo do seu cartão.
+
+Fui desenvolvido por @luisgbr1el.
+  
+*Versão 1.0.0*`
+  );
+});
+
+bot.launch();
+
+if (bot.launch) {
+  console.log("Bot online!");
+}
